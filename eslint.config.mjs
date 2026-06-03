d6699734a8ca4ae905aeff6ad5733f32a1d6d925
Viraj Shah <shahvirajd@gmail.com>
2026-06-03 01:53:50 -0400

    Tweak ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,29 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from "eslint/config"
+import nextVitals from "eslint-config-next/core-web-vitals"
+import nextTs from "eslint-config-next/typescript"
+import prettierConfig from "eslint-config-prettier"
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettierConfig,
+  {
+    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
+    rules: {
+      "arrow-parens": ["error", "as-needed"],
+      curly: ["error", "multi"],
+      "max-len": [
+        "error",
+        {
+          code: 180,
+          ignoreUrls: true,
+          tabWidth: 2,
+        },
+      ],
+      "no-tabs": "error",
+      semi: ["error", "never"],
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
@@ -13,6 +32,6 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
-]);
+])
 
-export default eslintConfig;
+export default eslintConfig
